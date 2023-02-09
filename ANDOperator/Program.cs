c1f7653e5f73e1db3f473ad2fc642d88f2c7d3eb
Darina Tšudakova <dtsudako@tktk.ee>
2023-02-09 22:16:32 +0200

    add week5 code

---
+++ ANDOperator/Program.cs
@@ -0,0 +1,22 @@
+﻿//Math >= 90; Biology >=90, Chemistry >=90;
+
+int math, biology, chemistry;
+
+Console.WriteLine("Enter yout Math result:");
+math = Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter yout Biology result:");
+biology= Int32.Parse(Console.ReadLine());
+
+Console.WriteLine("Enter yout Chemistry result:");
+chemistry= Int32.Parse(Console.ReadLine());
+
+
+if (math >= 90 && biology >= 90 && chemistry >= 90)
+{
+    Console.WriteLine("Congratulations! You got accepted.");
+}
+else
+{
+    Console.WriteLine("Your application cannot be approved.");
+}
